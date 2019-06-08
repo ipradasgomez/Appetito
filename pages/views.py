271b62ebe4341ab8ace597df00186d710b0c51fe
ipradasgomez <45384098+ipradasgomez@users.noreply.github.com>
2019-06-08 15:13:44 +0200

    Gestion de platos. Inicio pantalla búsqueda

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from plans.models import Plan
+from cartas.models import Plato
 # Create your views here.
 
 def about(request):
     return render(request, 'pages/about.html', {'nav_home': 'active'})
 
 def search(request):
-    return render(request, 'pages/search.html', {'nav_search': 'active'})
+    results = Plato.objects.filter(nombre__contains=request.GET['q'])
+    context={
+        'results': results
+    }
+    return render(request, 'pages/search.html', context)
 
 def landing(request):
     return render(request, 'pages/landing.html', {'nav_home': 'active', 'plans': Plan.objects.all()})
